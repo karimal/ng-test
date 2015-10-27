1c14deaacc975e0bec1be8a7fd1c95545e70e703
Karim Ali <karim.ali.dev@gmail.com>
2015-10-27 22:26:21 +0200

    Add functionality

--- js/bookLibrary.js
+++ js/bookLibrary.js
@@ -1,11 +1,12 @@
 angular.module('bookLibraryApp', [])
-  .config(function($locationProvider){
-    $locationProvider.html5Mode({ enabled: true, requireBase: false });
-  })
-  .controller('BooksListController', function($scope, $window ,$location, $timeout) {
-    var booksList = this;
-    booksList.state = 'add';
-    booksList.books = [
+.config(function($locationProvider){
+  $locationProvider.html5Mode({ enabled: true, requireBase: false });
+})
+.controller('BooksListController', function($scope, $window ,$location, $timeout) {
+  var booksList = this;
+  booksList.state = 'add';
+  if(!localStorage.getItem('booksList')){
+    var books = [
       {id:0, name:'The Cairo Trilogy',img:"https://d.gr-assets.com/books/1386924721l/5488.jpg", author:'Naguib Mahfouz', year: 1956},
       {id:1, name:'Kafka on the Shore',img:"https://d.gr-assets.com/books/1429638085l/4929.jpg", author:'Haruki Murakami', year: 2006},
       {id:2, name:'The New York Trilogy',img:"https://d.gr-assets.com/books/1386924429l/431.jpg", author:'Paul Auster', year: 1987},
@@ -14,37 +15,44 @@ angular.module('bookLibraryApp', [])
       {id:5, name:'One Hundred Years of Solitude',img:"https://d.gr-assets.com/books/1402721499l/22467482.jpg", author:'Gabriel Garcia Marquez', year: 1967},
       {id:6, name:'The Unbearable Lightness of Being',img:"https://d.gr-assets.com/books/1265401884l/9717.jpg", author:'Milan Kundera', year: 1984},
     ];
-
-    booksList.addBook = function() {
-      booksList.books.push({id: booksList.books.length ,name:booksList.book.name, author:booksList.book.author, year:booksList.book.year, price: booksList.book.price });
-      booksList.book = [];
-    };
-
-    booksList.selectBook = function(BookIndex) {
-      $window.location=      'edit.html?state=update&param=' + BookIndex;
-
-    // $location.url('edit.html').search('ref', 'test');
-    // $window.location.reload();
-
-      // booksList.book = {
-      //   id : booksList.books[BookIndex].id,
-      //   name : booksList.books[BookIndex].name,
-      //   author:booksList.books[BookIndex].author,
-      //   year:booksList.books[BookIndex].year,
-      //   price: booksList.books[BookIndex].price
-      // };
-      // booksList.state = 'select';
-    };
-
-    booksList.editBook = function(){
-      booksList.books[booksList.book.id] = booksList.book;
-      booksList.book = [];
-      booksList.state = 'add';
-    };
-
-    booksList.deleteBook = function() {
-
-    };
-}).controller('BooksEditController', function($scope, $window ,$location) {
+    localStorage.setItem('booksList', JSON.stringify(books));
+  }
+  booksList.books = JSON.parse(localStorage.getItem('booksList'));
+  booksList.selectBook = function(BookIndex) {
+    $window.location = 'edit.html?state=update&param=' + BookIndex;
+  };
+})
+.controller('BooksEditController', function($scope, $window ,$location) {
   $scope.state = $location.search().state;
+  $scope.books = JSON.parse(localStorage.getItem('booksList'));
+  $scope.saveBook = function() {
+    localStorage.setItem('booksList', JSON.stringify($scope.books));
+    $scope.book = [];
+    $window.location= 'index.html';
+  };
+  $scope.addBook = function(){
+    $scope.books.push({id: $scope.books.length ,name:$scope.book.name, author:$scope.book.author, img: $scope.book.img, year:$scope.book.year});
+    $scope.saveBook();
+  };
+  if($scope.state === 'update'){
+    $scope.param = $location.search().param;
+    $scope.book = {
+      id : $scope.books[$scope.param].id,
+      name : $scope.books[$scope.param].name,
+      author:$scope.books[$scope.param].author,
+      img: $scope.books[$scope.param].img,
+      year:$scope.books[$scope.param].year
+    }
+  }
+  $scope.updateBook = function() {
+    $scope.books[$scope.param] = $scope.book;
+    $scope.saveBook();
+  };
+  $scope.removeBook = function() {
+    $scope.books.splice($scope.param, 1);
+    $scope.saveBook();
+  };
+  $scope.backHome = function() {
+    $window.location= 'index.html';
+  };
 })
